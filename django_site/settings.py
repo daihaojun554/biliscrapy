2207ff4d9b77762c0db8a1d8b34c32c1ffad7bb3
daihaojun <1842118776@qq.com>
2023-11-20 21:05:50 +0800

    update:允许局域网访问

--- django_site/settings.py
+++ django_site/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-^67_9uv@&t(sb+!gv2l9bf@ypxbis%#&!qbcm3084dn+d7ke4k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
