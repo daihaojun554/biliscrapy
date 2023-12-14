382044c3e906b7e2f5b44199435245e3dec9c60a
daihaojun <1842118776@qq.com>
2023-12-14 16:32:38 +0800

    update:日志优化

--- biliscrapy/network/bilibili_utils.py
+++ biliscrapy/network/bilibili_utils.py
@@ -17,6 +17,13 @@ import logging
 class bili_utils:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+        self.logger.info("初始化完成")
     def bv_get(self, bvorurl):
         # https://api.bilibili.com/x/web-interface/view?bvid=BV1uG41197Tf
         # 将bv提取出来
