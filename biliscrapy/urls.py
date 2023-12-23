6d531db9af11abddafcf4c9f182f01504c711c40
daihaojun <daihaojun554@gmail.com>
2023-12-23 13:32:10 +0800

    新增parse_video 和download_video两个路由

--- biliscrapy/urls.py
+++ biliscrapy/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("flash_cookies", views.reflash_cookie, name='flash_cookies'),
     path('generate-chart/', views.generate_chart, name='generate_chart'),
     path('enter_card.html', views.enter_card, name='enter_card'),
-    path('export_data/',views.export_data,name='export_data'),
-    path('exit_card',views.exit_card,name='exit_card'),
-    path('download_video',views.download_video,name='download_video')
+    path('export_data/', views.export_data, name='export_data'),
+    path('exit_card', views.exit_card, name='exit_card'),
+    path('parse_video', views.parse_video, name='parse_video'),
+    path('download_video.html', views.download_video, name='download_video')
 ]
