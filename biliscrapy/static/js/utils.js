2b04cfc001041c5e3080765cdfa03728bd57a8c7
daihaojun <daihaojun554@gmail.com>
2023-12-23 23:47:41 +0800

    generateRandomFileName，getCookie两方法

---
+++ biliscrapy/static/js/utils.js
@@ -0,0 +1,26 @@
+// 获取 cookie 的值
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function generateRandomFileName(length) {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var fileName = '';
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * characters.length);
+        fileName += characters.charAt(randomIndex);
+    }
+
+    return fileName;
+}
